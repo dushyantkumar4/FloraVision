c88765d1647538f09ab352540cdeffcb2c60028b
dushyant kumar <dushyantvelar@gmail.com>
2025-10-31 23:47:17 +0530

    fix the but of import

--- src/views/OurBests.tsx
+++ src/views/OurBests.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import OurBest from '../components/OurBest'
 import Title from '../components/Title'
 
-const OurBests = () => {
+const OurBests:React.FC = () => {
   return (
     <div className='flex flex-col items-center gap-20 pr-8 pl-10 py-10'>
       <Title title={"Our Best o2"}/>
